9414e5576481859308b94341ec653e958776848e
alejandroileal <aleal1202@outlook.com>
2023-10-30 19:24:32 +0100

    Add html in modules

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,5 +1,17 @@
-export default function App() {
+import React from 'react';
+import { Info } from '../info/info';
+import { Button } from '../button/button';
+import { Gentleman } from '../gentleman/gentleman';
+
+export function App() {
   console.log('Loaded Main 😺');
 
-  return '';
+  return (
+    <div className="container">
+      <Info></Info>
+      <Button></Button>
+      <Gentleman></Gentleman>
+      <p>Loaded Kitty 😺</p>
+    </div>
+  );
 }
